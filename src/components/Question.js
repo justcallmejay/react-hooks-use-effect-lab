6ba9fba74e8441795cc5afbd38bc3ef568d214a4
jay <jaypifiverr@gmail.com>
2022-11-08 10:44:07 -0500

    commit tree 81987d43a420f5728f5f3428bb9f6eee69862a27

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+useEffect(() => {
+
+const timerID = setTimeout(() => 
+setTimeRemaining((timeRemaining) => timeRemaining - 1), 1000);
+    if (timeRemaining === 0) {
+      document.querySelector('h5').textContent = 'Game Over! Time ran out!'
+    };
+
+    return function iLikeEggs() {
+      clearTimeout(timerID)
+    }
+},[onAnswered(false), timeRemaining]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
